7fd9051d1522c6c58368c1a548d4a4b5ed7757eb
Marcelo Morais <m2marcelo@gmail.com>
2020-06-23 20:26:57 +0200

    Adding first draft for create todo
    Signed-off-by: Marcelo Morais <m2marcelo@gmail.com>

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,12 +1,36 @@
 import 'source-map-support/register'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
 
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import { getUserId } from '../utils'
+import { createTodo } from '../../businessLogic/todo'
+import { createLogger } from '../../utils/logger'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const newTodo: CreateTodoRequest = JSON.parse(event.body)
+const logger = createLogger('auth')
 
-  // TODO: Implement creating a new TODO item
-  return undefined
-}
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const newTodo: CreateTodoRequest = JSON.parse(event.body)
+    const userId = getUserId(event)
+
+    logger.info(`Received request for creating todo item for user ${userId}...`)
+
+    const item = createTodo(newTodo, userId)
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify({
+        item
+      })
+    }
+  }
+)
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
