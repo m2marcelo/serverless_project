ee8ea5d36ded8b0d55a0a7ab57c534a5bada337d
Marcelo Morais <m2marcelo@gmail.com>
2020-06-23 20:50:22 +0200

    Adding delete todo
    Signed-off-by: Marcelo Morais <m2marcelo@gmail.com>

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,10 +1,38 @@
 import 'source-map-support/register'
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import { getUserId } from '../utils'
+import { deleteTodo } from '../../businessLogic/todo'
+import { createLogger } from '../../utils/logger'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+const logger = createLogger('auth')
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const todoId = event.pathParameters.todoId
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const todoId = event.pathParameters.todoId
+    const userId = getUserId(event)
 
-  // TODO: Remove a TODO item by id
-  return undefined
-}
+    if (!todoId) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: 'Missing todoId' })
+      }
+    }
+
+    logger.info(`Received request to delete item=${todoId}, user=${userId}...`)
+
+    await deleteTodo(userId, todoId)
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({})
+    }
+  }
+)
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
