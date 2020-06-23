14dd4889293d7caf817004b1f105c0889100d252
Marcelo Morais <m2marcelo@gmail.com>
2020-06-23 20:09:13 +0200

    Updating Auth0 data
    Signed-off-by: Marcelo Morais <m2marcelo@gmail.com>

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -12,7 +12,7 @@ const logger = createLogger('auth')
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = 'https://dev-m2marcelo.eu.auth0.com/.well-known/jwks.json'
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -61,7 +61,21 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  if(!jwt){
+    throw new Error('invalid token')
+  }
+
+  try {
+    const response = await Axios.get(jwksUrl);
+    console.log(response);
+    var verifedToken = verify(token,response.data,{algorithms:['RS256']})
+
+    console.log('Verified token = ',verifedToken)
+    return  verifedToken as JwtPayload
+  } catch (error) {
+    console.error(error);
+    return undefined
+  }
 }
 
 function getToken(authHeader: string): string {
