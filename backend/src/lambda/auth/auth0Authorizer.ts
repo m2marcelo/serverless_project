aed7a564e09b45d390a96997413a5746a83dea41
Marcelo Morais <m2marcelo@gmail.com>
2020-06-24 22:17:30 +0200

    Updating authorizer
    Signed-off-by: Marcelo Morais <m2marcelo@gmail.com>

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,10 +1,9 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
-import { verify, decode } from 'jsonwebtoken'
+import { verify } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
 import Axios from 'axios'
-import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
@@ -55,27 +54,16 @@ export const handler = async (
 }
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
+  logger.info('Verifying token...')
+
   const token = getToken(authHeader)
-  const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  if(!jwt){
-    throw new Error('invalid token')
-  }
+  const res = await Axios.get(jwksUrl)
 
-  try {
-    const response = await Axios.get(jwksUrl);
-    console.log(response);
-    var verifedToken = verify(token,response.data,{algorithms:['RS256']})
+  const pemData = res['data']['keys'][0]['x5c'][0]
+  const cert = `-----BEGIN CERTIFICATE-----\n${pemData}\n-----END CERTIFICATE-----`
 
-    console.log('Verified token = ',verifedToken)
-    return  verifedToken as JwtPayload
-  } catch (error) {
-    console.error(error);
-    return undefined
-  }
+  return verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload
 }
 
 function getToken(authHeader: string): string {
