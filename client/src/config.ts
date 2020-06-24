6014e8ced68e9eb69fc763409222aa69d15e7223
Marcelo Morais <m2marcelo@gmail.com>
2020-06-24 22:42:13 +0200

    Updating client
    Signed-off-by: Marcelo Morais <m2marcelo@gmail.com>

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = '411b9hdg47'
+export const apiEndpoint = `https://${apiId}.execute-api.us-west-2.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-m2marcelo.eu.auth0.com',            // Auth0 domain
+  clientId: 'wetjGFkF7TL0d93WLhTC81O2otNP80NV',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
